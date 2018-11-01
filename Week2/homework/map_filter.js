5934820f9cc56e4c5afedb35bca20f7ae361080b
problemsolver72221 <problemsolver72221@gmail.com>
2018-11-01 19:44:39 +0100

    map-filter homework added

--- Week2/homework/map_filter.js
+++ Week2/homework/map_filter.js
@@ -1,5 +1,18 @@
 'use strict';
 
-const numbers = [1, 2, 3, 4];
+const numbers = [1, 2, 3, 4, 5, 6, 7];
 
 // Add your code here
+
+//Find the odds and assign them into a variable by using filter:
+
+const oddOnes = numbers.filter(i => i % 2 != 0)
+
+//Double the odds and kick out the evens:
+
+const oddDoubling = oddOnes.map(i => {
+    if (i % 2 != 0) {return i * 2;}
+    else {return i;}
+})
+
+console.log("Doubled lucky odds: " + oddDoubling + "\nUnlucky kicked evens: " +oddOnes);
